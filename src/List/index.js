a3120a93680a146b1ccba79f255edd6b0713a4de
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-06-12 17:38:49 -0300

    finishing list styling

--- src/List/index.js
+++ src/List/index.js
@@ -1,10 +1,85 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
-export default function List() {
+import styles from './styles';
+
+export default function List(props) {
+
+    function carregaIcone(likeada) {
+        return likeada ? require('../images/likeada.png') : require('../images/like.png')
+    };
+
+    function mostraLikes(likers) {
+        if(likers === 0) {
+            return;
+        };
+
+        return(
+            <Text style={styles.likes}>{likers} {likers > 1 ? 'curtidas' : 'curtida'}</Text>  
+        );
+    };
+
     return(
         <View>
-            <Text>Lista Feed</Text>
+            
+            <View style={styles.viewPerfil}>
+
+                <Image
+                    style={styles.fotoPerfil}
+                    source={{ uri: props.data.imgperfil }}
+                />
+
+                <Text style={styles.nomeUsuario}>{props.data.nome}</Text>
+
+            </View>
+
+            <Image
+                resizeMode="cover"
+                source={{ uri: props.data.imgPublicacao }}
+                style={styles.fotoPublication}
+            />
+
+            <View style={styles.areaBtn}>
+
+                <TouchableOpacity>
+
+                    <Image
+                        source={carregaIcone(props.data.likeada)}
+                        style={styles.iconLike}
+                    />
+
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnSend}>
+
+                    <Image
+                        source={require('../images/comment.png')}
+                        style={styles.iconLike}
+                    />
+
+                </TouchableOpacity>
+
+                <TouchableOpacity style={styles.btnSend}>
+
+                    <Image
+                        source={require('../images/send.png')}
+                        style={styles.iconLike}
+                    />
+
+                </TouchableOpacity>
+
+            </View>
+
+            {mostraLikes(props.data.likers)}
+
+            <Text style={styles.nomeRodape}>
+                {props.data.nome}
+            </Text>
+
+            <Text style={styles.descRodape}>
+                {props.data.descricao}
+            </Text>
+
         </View>
     );
 };
---
+++ src/List/styles.js
@@ -0,0 +1,51 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    viewPerfil:{
+        flexDirection: 'row',
+        flex: 1,
+        alignItems: 'center',
+        padding: 8
+    },
+    fotoPerfil:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+    },
+    nomeUsuario:{
+       paddingLeft: 5,
+       fontSize: 22,
+       color: '#000000',
+    },
+    fotoPublication:{
+        height: 400,
+        alignItems: 'center',
+    },
+    areaBtn:{
+        flexDirection: 'row',
+        padding: 5,
+    },
+    iconLike:{
+        width: 25,
+        height: 25,
+    },
+    btnSend:{
+        paddingLeft: 5,
+    },
+    likes:{
+        fontWeight: 'bold',
+        marginLeft: 5,
+    },
+    nomeRodape:{
+        fontSize: 18,
+        fontWeight: 'bold',
+        paddingLeft: 5,
+    },
+    descRodape:{
+        paddingLeft: 5,
+        paddingBottom: 10,
+        fontSize: 15,
+    },
+});
+
+export default styles;
