464b4cfae80ffc080ef92c3e9b328b52dd14c5df
Pedro Henrique Vicente Ferreira <anddev007pe@gmail.com>
2021-06-12 13:41:17 -0300

    created Header component

---
+++ src/Header/styles.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    header:{
+        height: 55,
+        backgroundColor: '#ffffff',
+    },
+    text:{
+        color: '#121212',
+    },
+});
+
+export default styles;
